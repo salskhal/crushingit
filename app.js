a7af3667d92974ff15ed7219afc7bd841842d53f
salskhal <olaniyisal@gmail.com>
2023-11-22 13:49:28 +0100

    Feat/ Added CTA section

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-    const userIcon = document.getElementById("user-dropdown");
-    const userDropdown = document.getElementById("user-dropdown-content");
+const userIcon = document.getElementById("user-dropdown");
+const userDropdown = document.getElementById("user-dropdown-content");
 
-    const notificationIcon = document.getElementById("notification-dropdown");
-    const notificationDropdown = document.getElementById(
-    "notification-dropdown-content"
-    );
+const notificationIcon = document.getElementById("notification-dropdown");
+const notificationDropdown = document.getElementById(
+  "notification-dropdown-content"
+);
 
-    userIcon.addEventListener("click", () => {
-    userDropdown.classList.toggle("show");
-    });
+userIcon.addEventListener("click", () => {
+  userDropdown.classList.toggle("show");
+});
 
-    notificationIcon.addEventListener("click", () => {
-    notificationDropdown.classList.toggle("show");
-    });
+notificationIcon.addEventListener("click", () => {
+  notificationDropdown.classList.toggle("show");
+});
 
-    // close user dropdown when notification dropdown is clicked
-    notificationIcon.addEventListener("click", () => {
-    userDropdown.classList.remove("show");
-    });
+// close user dropdown when notification dropdown is clicked
+notificationIcon.addEventListener("click", () => {
+  userDropdown.classList.remove("show");
+});
 
-    // close notification dropdown when user dropdown is clicked
-    userIcon.addEventListener("click", () => {
-    notificationDropdown.classList.remove("show");
-    });
+// close notification dropdown when user dropdown is clicked
+userIcon.addEventListener("click", () => {
+  notificationDropdown.classList.remove("show");
+});
 
+// cta button
+
+const ctaIcon = document.getElementById("ctaIcon");
+const cta = document.getElementById("cta");
+
+ctaIcon.addEventListener("click", () => {
+  cta.classList.add("close");
+});
